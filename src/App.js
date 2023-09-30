29b3b19565b42988961d475de499df0008eacdbb
NovaBam123 <143675556+NovaBam123@users.noreply.github.com>
2023-09-30 23:02:58 +0700

    adding sytle and commit button

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@
 function App() {
   return (
     <div>
-      <h1 className="text-4xl font-bold underline">Wellcome</h1>
+      <h1 className="text-4xl 
+      font-bold underline
+      font-roboto
+      ">Wellcome</h1>
       <h2 className="
       mt-4 text-blue-600 text-xl pl-2 
       ml-2 border-l-4
       border-red-500
+      rounded font-serif-times
       ">Hello Tailwind</h2>
+
+      <button className="
+      ml-10 mt-4
+      border-2 p-1 pl-2 pr-2
+      bg-purple-400 
+      rounded-lg 
+      font-medium
+      shadow-4xl
+      hover:bg-violet-300
+      ">Confirm</button>
     </div>
   );
 }

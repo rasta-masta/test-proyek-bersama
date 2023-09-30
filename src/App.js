5f21ef3560903a5e1477b253d515f44df0b0e793
rasta-masta <143915828+rasta-masta@users.noreply.github.com>
2023-09-30 22:08:15 +0700

    trying style with tailwind-css

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ function App() {
   return (
     <div>
       <h1 className="text-4xl font-bold underline">Wellcome</h1>
+      <h2 className="
+      mt-4 text-blue-600 text-xl pl-2 
+      ml-2 border-l-4
+      border-red-500
+      ">Hello Tailwind</h2>
     </div>
   );
 }
